c90402af9b57b9a7462372bbe2516d41f214026a
Keijiro Takahashi <keijiro@gmail.com>
2013-04-21 15:43:24 +0900

    Simplify the procedure.

--- Assets/ShutterSound.js
+++ Assets/ShutterSound.js
@@ -6,35 +6,17 @@ function Update() {
 	}
 }
 
-static function PlayShutterSound() {
+static private var mediaActionSound : AndroidJavaObject;
+
+static private function PlayShutterSound() {
 #if UNITY_ANDROID
 
-	var unityPlayer = AndroidJavaClass("com.unity3d.player.UnityPlayer");
-	var currentActivity = unityPlayer.GetStatic.<AndroidJavaObject>("currentActivity");
+	if (!mediaActionSound) {
+		mediaActionSound = AndroidJavaObject("android.media.MediaActionSound");
+	}
 
-	// AudioManager audioManager = currentActivity.getSystemService(Context.AUDIO_SERVICE);
-	var audioService = currentActivity.GetStatic.<String>("AUDIO_SERVICE");
-	var audioManager = currentActivity.Call.<AndroidJavaObject>("getSystemService", audioService);
-
-	// maxVolume = audioManager.getStreamMaxVolume(audioManager.STREAM_SYSTEM_ENFORCED);
-	var streamType : int = audioManager.GetStatic.<uint>("STREAM_SYSTEM_ENFORCED");
-	var maxVolume = audioManager.Call.<int>("getStreamMaxVolume", streamType);
-
-	// audioManager.setStreamVolume(audioManager.STREAM_SYSTEM_ENFORCED, maxVolume, 0);
-	audioManager.Call("setStreamVolume", streamType, maxVolume, 0);
-
-	// mediaPlayer = new MediaPlayer();
-	// mediaPlayer.setDataSource("file:///system/media/audio/ui/camera_click.ogg");
-	var mediaPlayer = AndroidJavaObject("android.media.MediaPlayer");
-	mediaPlayer.Call("setDataSource", "file:///system/media/audio/ui/camera_click.ogg");
-
-	// mediaPlayer.setAudioStreamType(audioManager.STREAM_SYSTEM_ENFORCED);
-	mediaPlayer.Call("setAudioStreamType", streamType);
-
-	// mediaPlayer.prepare();
-	// mediaPlayer.start();
-	mediaPlayer.Call("prepare");
-	mediaPlayer.Call("start");
+	var soundName : int = mediaActionSound.GetStatic.<uint>("SHUTTER_CLICK");
+	mediaActionSound.Call("play", soundName);
 
 #endif
 }
